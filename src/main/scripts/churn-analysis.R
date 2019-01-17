b0c410eaacc8a19eac5c8acd760487cafb51fc20
tEdFAn026 <a2857782qq@163.com>
2019-01-17 19:13:46 +0000

    Update churn-analysis.R

--- src/main/scripts/churn-analysis.R
+++ src/main/scripts/churn-analysis.R
@@ -2,5 +2,5 @@ library(plyr)
 library(ggplot2)
 
 diffs <- read.csv("diffs.csv")
-churnSummary <- ddply(diffs,.(File),summarise,Add=sum(Added), Rem=sum(Removed), TotalChurn = Add+Rem, Edits=length(File))
+churnSummary <- ddply(diffs,.(File),summarise,Add=sum(as.numeric(Added)), Rem=sum(as.numeric(Removed)), TotalChurn = Add+Rem, Edits=length(File))
 ggplot(churnSummary,aes(x=Add,y=Rem)) + geom_point(aes(size=Edits)) + geom_text(data=churnSummary[churnSummary$TotalChurn>500,],aes(label=File),nudge_x = 40)
